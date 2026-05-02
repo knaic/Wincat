870b8d4d50db9428b90e2e5e46e51caee47f7864
knaic <tux@xyecoc.com>
2026-05-02 12:13:01 +1000

    Added

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Wincat
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            openToolStripMenuItem = new ToolStripMenuItem();
+            button1 = new Button();
+            button2 = new Button();
+            trackBar1 = new TrackBar();
+            label1 = new Label();
+            menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(280, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.Size = new Size(103, 22);
+            openToolStripMenuItem.Text = "Open";
+            openToolStripMenuItem.Click += openToolStripMenuItem_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(93, 367);
+            button1.Name = "button1";
+            button1.Size = new Size(87, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Play";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(40, 367);
+            button2.Name = "button2";
+            button2.Size = new Size(47, 23);
+            button2.TabIndex = 2;
+            button2.Text = "Stop";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // trackBar1
+            // 
+            trackBar1.Location = new Point(184, 367);
+            trackBar1.Maximum = 100;
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(85, 45);
+            trackBar1.TabIndex = 3;
+            trackBar1.TickFrequency = 10;
+            trackBar1.Value = 50;
+            trackBar1.Scroll += trackBar1_Scroll;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Yu Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label1.Location = new Point(57, 343);
+            label1.Name = "label1";
+            label1.Size = new Size(156, 21);
+            label1.TabIndex = 4;
+            label1.Text = "Now Playing : None";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(280, 397);
+            Controls.Add(label1);
+            Controls.Add(trackBar1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Wincat";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private Button button1;
+        private Button button2;
+        private TrackBar trackBar1;
+        private Label label1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+using System.Windows.Forms;
+using NAudio.Wave;
+
+namespace Wincat
+{
+
+    public struct User
+    {
+        public String Path;
+    }
+    public partial class Form1 : Form
+    {
+        User u;
+
+        private AudioFileReader? audioreader;
+        private WaveOutEvent? OutputDevice;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var dialogmenu = new OpenFileDialog();
+            dialogmenu.Filter = "MP3 Files|*.mp3";
+            if (dialogmenu.ShowDialog() == DialogResult.OK)
+            {
+                u.Path = dialogmenu.FileName;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(u.Path))
+            {
+                MessageBox.Show("No file was selected.");
+                return;
+            }
+            else
+            {
+                label1.Text = System.IO.Path.GetFileName(u.Path);
+                OutputDevice?.Stop();
+                OutputDevice?.Dispose();
+                audioreader?.Dispose();
+
+                audioreader = new AudioFileReader(u.Path);
+                OutputDevice = new WaveOutEvent();
+
+                OutputDevice.Init(audioreader);
+                OutputDevice.Play();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Now Playing : None";
+            OutputDevice?.Stop();
+            OutputDevice?.Dispose();
+            audioreader?.Dispose();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if (audioreader != null)
+            {
+                audioreader.Volume = trackBar1.Value / 100f;
+            }
+        }
+    }
+}
